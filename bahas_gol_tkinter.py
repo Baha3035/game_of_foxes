93596ebea03ac5ad90d2c1d070c1fe07a7a65bb9
Baha <batmaaanpls@gmail.com>
2022-04-05 18:05:17 +0600

    lint main game of life class

--- bahas_gol_tkinter.py
+++ bahas_gol_tkinter.py
@@ -3,8 +3,11 @@ from random import randint, choice
 from fox import Fox
 from grass import Grass
 from rabbit import Rabbit
-import fox, grass, rabbit
- 
+import fox
+import grass
+import rabbit
+
+
 class Field:
     def __init__(self, c, n, m, width, height, fox, rabbit, grass):
         '''
@@ -28,31 +31,35 @@ class Field:
             self.a.append([])
             for j in range(self.m):
                 self.a[i].append(0)
-        self.a = [[randint(0,2) for i in range(width)] for j in range(height)]
+        self.a = [[randint(0, 2) for i in range(width)] for j in range(height)]
         # 1 - Fox (orange)
         # 2 - Rabbit (blue)
         # 0 - Grass (green)
         self.draw()
-    #helper check all its neighbors into temporary (tmp) array
-    def helper(self,r,c):
-        M,N = self.n, self.m
+    # helper check all its neighbors into temporary (tmp) array
+
+    def helper(self, r, c):
+        M, N = self.n, self.m
         cnt_foxes, cnt_rabbits = 0, 0
-        coordinates = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
-        for x,y in coordinates:
+        coordinates = [(0, 1), (0, -1), (1, 0), (-1, 0),
+                       (1, 1), (1, -1), (-1, 1), (-1, -1)]
+        for x, y in coordinates:
             if 0 <= r + y < M and 0 <= c + x < N and self.a[r+y][x+c] == 1:
                 cnt_foxes += 1
             elif 0 <= r + y < M and 0 <= c + x < N and self.a[r+y][x+c] == 2:
-                cnt_rabbits +=1
-            
+                cnt_rabbits += 1
+
         if self.a[r][c] == 2:
-            if cnt_foxes > 1: # if foxes are at least 2 and there is one rabbit they can eat it and reproduce to its place
+            if cnt_foxes > 1:
                 return 1
+# if foxes are at least 2 and there is one rabbit
+# they can eat it and reproduce to its place
             elif 2 <= cnt_rabbits <= 3:
                 return 2
         elif self.a[r][c] == 1:
             if cnt_foxes < 2:
                 return 0
-            elif 2 <= cnt_foxes <=3 and cnt_rabbits > 1:
+            elif 2 <= cnt_foxes <= 3 and cnt_rabbits > 1:
                 return 1
             else:
                 return 0
@@ -61,20 +68,21 @@ class Field:
                 return 2
             else:
                 return 0
+
     def step(self):
-        tmp = [[None]  * self.m for _ in range(self.n)]
+        tmp = [[None] * self.m for _ in range(self.n)]
         for r in range(self.n):
             for c in range(self.m):
-                tmp[r][c] = self.helper(r,c) # temporary array filled with next moves of animals
+                tmp[r][c] = self.helper(r, c)
+                # temporary array filled with next moves of animals
         self.a = tmp
- 
- 
+
     def print_field(self):
         for i in range(self.n):
             for j in range(self.m):
                 print(self.a[i][j], end="")
             print()
- 
+
     def draw(self):
         color = "grey"
         sizen = self.width // (self.n - 2)
@@ -82,15 +90,16 @@ class Field:
         for i in range(1, self.n - 1):
             for j in range(1, self.m - 1):
                 if (self.a[i][j] == 1):
-                    color = self.fox.get_color() # fox
+                    color = self.fox.get_color()  # fox
                 elif (self.a[i][j] == 2):
-                     color = self.rabbit.get_color() # rabbit
+                    color = self.rabbit.get_color()  # rabbit
                 else:
-                    color = self.grass.get_color() # grass
-                self.c.create_rectangle((i-1) * sizen, (j-1) * sizem, (i) * sizen, (j) * sizem, fill=color)
+                    color = self.grass.get_color()  # grass
+                self.c.create_rectangle((i-1) * sizen, (j-1) * sizem,
+                                        (i) * sizen, (j) * sizem, fill=color)
         self.step()
         self.c.after(2000, self.draw)
- 
+
 root = Tk()
 root.geometry("800x800")
 c = Canvas(root, width=800, height=800)
@@ -99,16 +108,13 @@ fox, rabbit, grass = None, None, None
 try:
     fox = Fox()
     rabbit = Rabbit()
-    grass = Grass()      # Exception handling to ensure that objects are properly initialized
+    grass = Grass()
+    # Exception handling to ensure that objects are properly initialized
 except:
     print("objects are not initialized")
-#fox = Fox()
-#rabbit = Rabbit()
-#grass = Grass() 
 f = Field(c, 40, 40, 800, 800, fox, rabbit, grass)
 cntt = 0
 f.print_field()
 cntt += 1
 print(cntt)
 root.mainloop()
-# c.create_oval(p.x + 5,p.y + 5,p.x+p.size + 5,p.y+p.size + 5)
